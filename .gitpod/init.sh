1e52d62ea863dad7844d5e21f549872e8b0966df
gounthar <gounthar@gmail.com>
2024-07-09 21:29:09 +0200

    fix(gitpod): If you come back, the test-plugins dir already exists.

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -11,7 +11,7 @@ mvn clean install
 
 # Create a directory for the test plugins and move into it. This directory will serve
 # as a workspace for cloning and building Jenkins plugin repositories.
-mkdir test-plugins && cd test-plugins || exit
+mkdir -p test-plugins && cd test-plugins || exit
 
 # Clone the first Jenkins plugin repository. The badge-plugin is a Jenkins plugin that
 # adds badges to build pages, indicating various conditions like coverage or build status.
