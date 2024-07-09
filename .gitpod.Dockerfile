e018bcfa7e856b87b8599171629391ba7ab1cfda
gounthar <gounthar@gmail.com>
2024-07-09 18:51:24 +0200

    fix(gitpod): Documentation.

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -6,7 +6,9 @@
 FROM gitpod/workspace-java-17
 
 # Update the package list and install Maven. Maven is required for building Java projects, including Jenkins plugins.
+# The `apt-get update` command updates the list of available packages and their versions, but it does not install or
+# upgrade any packages. The `apt-get install -y maven` command installs Maven without requiring confirmation from the user.
+# Finally, `rm -rf /var/lib/apt/lists/*` cleans up the package list files to keep the Docker image size down.
 RUN sudo apt-get update && \
     sudo apt-get install -y maven && \
     sudo rm -rf /var/lib/apt/lists/*
-
--- init.sh
+++ init.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+# This script is designed to set up the environment for a Java project using Maven.
+# It performs a clean install of the project dependencies and sets up a directory
+# for Jenkins plugin development by cloning necessary repositories.
+
 # Install dependencies and perform initial setup
 
-# Run Maven clean install
+# Run Maven clean install to ensure all project dependencies are correctly installed
+# and the project is built successfully.
 mvn clean install
 
-# Create a directory for the test plugins and move into it. This directory will hold the cloned repositories.
+# Create a directory for the test plugins and move into it. This directory will serve
+# as a workspace for cloning and building Jenkins plugin repositories.
 mkdir test-plugins && cd test-plugins || exit
 
-# Clone the first Jenkins plugin repository. This plugin will be built as part of the Docker image.
+# Clone the first Jenkins plugin repository. The badge-plugin is a Jenkins plugin that
+# adds badges to build pages, indicating various conditions like coverage or build status.
 git clone https://github.com/jenkinsci/badge-plugin.git
 
-# Clone the second Jenkins plugin repository. This is another plugin that will be built.
+# Clone the second Jenkins plugin repository. The build-timestamp-plugin adds the ability
+# to display build timestamps in the Jenkins UI. This plugin will also be built as part
+# of the Docker image setup.
 git clone https://github.com/jenkinsci/build-timestamp-plugin.git
