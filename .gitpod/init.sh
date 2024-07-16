b0a4e82406f97440354939f67286f5d1c7fe9959
gounthar <gounthar@gmail.com>
2024-07-16 11:26:53 +0200

    fix(gitpod): Setting GH_OWNER too.

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -46,19 +46,22 @@ fi
 # Check if the terminal supports colors
 if tput colors > /dev/null 2>&1; then
   color_cyan="\033[36m"
-  color_blue="\033[34m" # Blue color definition
+  color_blue="\033[34m"
+  color_green="\033[01;32m"
   color_reset="\033[0m"
 else
   color_cyan=""
   color_blue=""
+  color_green=""
   color_reset=""
 fi
 
-echo -e "Hello $GITPOD_GIT_USER_NAME"
+echo -e "Hello ${color_green}$GITPOD_GIT_USER_NAME${color_reset} 👋"
 echo -e "This tool needs a GitHub token to work."
 echo -e "GitPod created a temporary GitHub token for you: ${color_cyan}$SCM_TOKEN${color_reset}"
-echo -e "If that's fine with you, we'll store it in an environment variable so the tool can use it."
- echo -e "Please enter this command in the terminal: ${color_cyan}export GH_TOKEN=$SCM_TOKEN${color_reset}"
+echo -e "We stored it in the ${color_cyan}GH_TOKEN${color_reset} environment variable."
+#echo -e "If that's fine with you, we'll store it in an environment variable so the tool can use it."
+#echo -e "Please enter this command in the terminal: ${color_cyan}export GH_TOKEN=$SCM_TOKEN${color_reset}"
 echo -e "If you want to use a different token, please set it in the ${color_cyan}GH_TOKEN${color_reset} environment variable."
 echo -e "If you want to know more about GitHub tokens, please visit ${color_blue}https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens${color_reset}"
 GH_TOKEN="$SCM_TOKEN"
@@ -66,9 +69,11 @@ export GH_TOKEN
 # Make a GET request to the GitHub API to fetch user information
 response=$(curl -sH "Authorization: token $SCM_TOKEN" https://api.github.com/user)
 # Extract the GitHub handle from the response
-handle=$(echo $response | jq -r '.login')
-export handle
-echo "GitHub handle: $handle"
+GH_OWNER=$(echo $response | jq -r '.login')
+export GH_OWNER
+echo -e "This tool needs a GitHub handle to clone and fork plugin repositories"
+echo -e "We stored ${color_green}$GH_OWNER${color_reset} in the ${color_cyan}GH_OWNER${color_reset} environment variable."
+echo -e "If you want to use another GitHub handle, please set the ${color_green}$GH_OWNER${color_reset} accordingly."
 echo -e "As a gentle reminder, we have already cloned two Jenkins plugin repositories: ${color_cyan}badge-plugin${color_reset} and ${color_cyan}build-timestamp-plugin${color_reset}."
 echo -e "You can now proceed with the modernizer tool thanks to the following commands:"
 echo -e "${color_cyan}java -jar plugin-modernizer-cli/target/jenkins-plugin-modernizer-999999-SNAPSHOT.jar --plugins badge-plugin,build-timestamp-plugin --recipes AddPluginsBom,AddCodeOwner${color_reset}"
