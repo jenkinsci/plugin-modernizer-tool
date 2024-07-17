af15f07b0115bb9091493f7323c4b64606392bcf
sridamul <sridamul@gmail.com>
2024-07-17 12:18:40 +0530

    Add unit tests

---
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/impl/CacheManagerTest.java
@@ -0,0 +1,36 @@
+package io.jenkins.tools.pluginmodernizer.core.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CacheManagerTest {
+
+    @TempDir
+    Path tempDir;
+
+    @Test
+    void testCreateCacheWhenNotExists() throws IOException {
+        Path cachePath = tempDir.resolve("cache");
+        assertFalse(Files.exists(cachePath));
+        CacheManager cacheManager = new CacheManager(cachePath);
+        cacheManager.createCache();
+        assertTrue(Files.exists(cachePath));
+    }
+
+    @Test
+    void testCreateCacheWhenExists() throws IOException {
+        Path cachePath = tempDir.resolve("cache");
+        Files.createDirectories(cachePath);
+        CacheManager cacheManager = new CacheManager(cachePath);
+        cacheManager.createCache();
+        assertTrue(Files.exists(cachePath));
+    }
+}
