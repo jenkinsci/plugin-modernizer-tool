65b76f159ffd3749774d0e4a145ff787431615de
sridamul <sridamul@gmail.com>
2024-07-17 14:38:59 +0530

    Add NoSuchFileException

--- plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/utils/PluginListParser.java
+++ plugin-modernizer-core/src/main/java/io/jenkins/tools/pluginmodernizer/core/utils/PluginListParser.java
@@ -2,6 +2,7 @@ package io.jenkins.tools.pluginmodernizer.core.utils;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +20,9 @@ public class PluginListParser {
             return lines.filter(line -> !line.trim().isEmpty())
                         .map(line -> line.split(":")[0])
                         .collect(Collectors.toList());
+        } catch (NoSuchFileException e) {
+            LOG.error("File not found: {}", pluginFile);
+            return null;
         } catch (IOException e) {
             LOG.error("Error reading plugins from file: {}", e.getMessage());
             return null;
